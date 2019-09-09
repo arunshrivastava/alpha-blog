c95175e32faf25f672048d7fa8fe620140bc79a5
pankaj <rupesh.saklecha@karyonsolutions.com>
2019-09-09 14:55:11 +0530

    create model, table and validations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #https://gentle-depths-86735.herokuapp.com/
   root 'pages#home'
   get 'about', to: 'pages#about'
+
 end
