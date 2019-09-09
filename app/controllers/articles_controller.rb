7c24023af0ecebcfdfc083201ba08f3b283ec6d2
pankaj <rupesh.saklecha@karyonsolutions.com>
2019-09-09 16:26:44 +0530

    Showing article data, add flash message

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,8 +5,16 @@ class ArticlesController < ApplicationController
 	end
   def create
 		@article = Article.new(article_prams)
-		@article.save
-		redirect_to article_show(@article)
+	  if @article.save
+			flash[:notic] = "Article was sucessfully created"
+			redirect_to article_path(@article)
+	else
+		render :new
+		end
+	end
+
+  def show
+		@article = Article.find(params[:id])
 	end
 
   private
